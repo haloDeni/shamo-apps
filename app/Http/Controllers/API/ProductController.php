f0a3f6263c84b3bcd33601534b36d0639f78c4c7
Deni Nuryadi <Deninuryadi63@gmail.com>
2021-10-18 20:24:59 +0700

    Komit Pertama

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function all(Request $request){
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $tags = $request->input('tags');
+        $categories =  $request->input('categories');
+        $price_form =  $request->input('price_form');
+        $price_to =  $request->input('price_to');
+
+
+        if($id){
+            $product = Product::with(['category','galleries'])->find($id);
+            
+        }
+    }
+}
